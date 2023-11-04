fc3ba28c87c087c5c0ee72727147b96907303afa
Ben <106643280+ben745260@users.noreply.github.com>
2023-11-04 22:13:51 +0800

    Init

--- main_2.py
+++ main_2.py
@@ -7,10 +7,10 @@ import nltk
 from string import punctuation
 import re
 from wordcloud import WordCloud
-from transformers import pipeline
-import os
 from transformers import BertForSequenceClassification, BertTokenizer, AdamW
 import torch
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from sklearn.model_selection import train_test_split
 
 warnings.filterwarnings('ignore')
 
@@ -78,6 +78,7 @@ df['clean_review'] = df['Review'].apply(clean_text)
 df.drop("Review", axis=1, inplace=True)
 df.to_csv('Src/shoe_cleanData.csv', index=False)
 
+
 # ================================================================
 # 3. Model deployment
 # Sentiment analysis using BERT model 
@@ -87,59 +88,66 @@ model_name = 'bert-base-uncased'
 model = BertForSequenceClassification.from_pretrained(model_name)
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
-# Define a function to perform sentiment analysis using BERT
-def analyze_sentiment(review):
-    inputs = tokenizer.encode_plus(
-        review,
-        add_special_tokens=True,
-        max_length=512,
-        padding='max_length',
-        truncation=True,
-        return_tensors='pt'
-    )
-    input_ids = inputs['input_ids']
-    attention_mask = inputs['attention_mask']
+# Split the data into training and validation sets
+train_df, val_df = train_test_split(df, test_size=0.2)
+
+# Tokenize the input reviews for both training and validation sets
+train_encodings = tokenizer(list(train_df['clean_review']), truncation=True, padding=True)
+val_encodings = tokenizer(list(val_df['clean_review']), truncation=True, padding=True)
+
+# Create PyTorch datasets
+class ReviewDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = ReviewDataset(train_encodings, list(train_df['Review_rating']))
+val_dataset = ReviewDataset(val_encodings, list(val_df['Review_rating']))
+
+# Create data loaders
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+
+# Set up the optimizer and scheduler
+optimizer = AdamW(model.parameters(), lr=1e-5)
+epochs = 10
+
+# Training loop
+for epoch in range(epochs):
+    model.train()
+    train_loss = 0.0
+    for batch in train_loader:
+        optimizer.zero_grad()
+        inputs = {key: val for key, val in batch.items()}
+        outputs = model(**inputs)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
     
+    model.eval()
+val_loss = 0.0
     with torch.no_grad():
-        outputs = model(input_ids, attention_mask=attention_mask)
-        logits = outputs.logits
-        probabilities = torch.softmax(logits, dim=1).tolist()[0]
+        for batch in val_loader:
+            inputs = {key: val for key, val in batch.items()}
+            outputs = model(**inputs)
+            loss = outputs.loss
+            val_loss += loss.item()
     
-    sentiment = 'Positive' if probabilities[1] > probabilities[0] else 'Negative'
-    return sentiment
-
-# Apply sentiment analysis to the 'clean_review' column
-df['Sentiment'] = df['clean_review'].apply(analyze_sentiment)
-
-# Save the sentiment analysis results to a CSV file
-df.to_csv('Src/sentiment_analysis_results.csv', index=False)
-
-# ================================================================
-# 4. Recommendations based on sentiment analysis
-# Group the DataFrame by product_id
-grouped_df = df.groupby('Product_id')
-
-recommendations = []
-
-for product_id, group in grouped_df:
-    positive_reviews = group[group['Sentiment'] == 'Positive']
-    positive_review_count = len(positive_reviews)
+    avg_train_loss = train_loss / len(train_loader)
+    avg_val_loss = val_loss / len(val_loader)
     
-    negative_reviews = group[group['Sentiment'] == 'Negative']
-    negative_review_count = len(negative_reviews)
-    
-    total_review_count = len(group)
-    
-    recommendation = {
-        'Product_id': product_id,
-        'Positive_Review_Count': positive_review_count,
-        'Negative_Review_Count': negative_review_count,
-        'Total_Review_Count': total_review_count
-    }
-    
-    recommendations.append(recommendation)
+    print(f"Epoch {epoch+1}: Train Loss = {avg_train_loss:.4f}, Val Loss = {avg_val_loss:.4f}")
 
-recommendations_df = pd.DataFrame(recommendations)
+# Save the trained model
+model.save_pretrained('Models/bert_sentiment_analysis')
 
-# Save the recommendations to a CSV file
-recommendations_df.to_csv('Src/recommendations_results.csv', index=False)
+# Once the model is trained, you can use it for predictions and inference on new data.
